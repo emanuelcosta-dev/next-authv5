5ea1da7d62ed3ff388e56cc9aaeb8c92a8f5ac4f
Emanuel Costa <emanuelcosta28@outlook.com>
2024-01-17 19:53:26 +0000

    Add basic file structure

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <nav className="bg-black text-white">Shared navbar</nav>
+      {children}
+    </div>
+  );
+}
