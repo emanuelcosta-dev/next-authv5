6dde3f4116a90dc6fa5197b1fc4c1d798401282d
Emanuel Costa <emanuelcosta28@outlook.com>
2024-01-18 15:50:33 +0000

    Basic auth page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button>Click me!</Button>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-slate-900">
+      <div className="space-y-6">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          Auth
+        </h1>
+        <p className="text-white text-lg">Next Authentication service</p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ components/auth/login-button.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+interface LoginButtonProps {
+  children: React.ReactNode;
+  mode?: "modal" | "redirect";
+  asChild?: boolean;
+}
+
+export const LoginButton = ({
+  children,
+  mode = "redirect",
+  asChild,
+}: LoginButtonProps) => {
+  const onClick = () => {
+    console.log("LOGIN BUTTON CLICKED");
+  };
+
+  if (mode == "modal") {
+    return <span>TODO: Implement modal</span>;
+  }
+  return (
+    <span onClick={onClick} className="cursor-pointer">
+      {children}
+    </span>
+  );
+};
