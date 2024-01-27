b179efb4ecd8d92c824e1e3f164ae98917adc711
Emanuel Costa <emanuelcosta28@outlook.com>
2024-01-27 18:01:03 +0000

    Add bcrypt

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
